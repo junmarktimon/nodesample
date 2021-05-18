28269b236a30879fd65b5e9b36f297a9bc8c365d
junmarktimon <doymakey@gmail.com>
2021-05-18 13:26:32 +0800

    4th commit

--- index.js
+++ index.js
@@ -7,19 +7,27 @@
 // app.listen(process.env.PORT || port, () => console.log('Node server running at http://localhost:${port}'));
 
 
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const router = express.Router();
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
+// router.get('/',function(req,res){
+//   res.sendFile(path.join(__dirname+'/index.html'));
+//   //__dirname : It will resolve to your project folder.
+// });
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen('3000');
+console.log('working on 3000');
 
 
 //add the router
-app.use('/', router);
-app.listen(process.env.port || 3000);
+// app.use('/', router);
+// app.listen(process.env.port || 3000);
 
-console.log('Running at Port 3000');
+// console.log('Running at Port 3000');
